db63c457fbbce43c358a598b31cf8cbd114ff01d
Thomas Bridge <thomas.bridge@icloud.com>
2022-02-11 08:27:36 +0000

    Complete Module 1

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,16 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController {
-
+    @RequestMapping("/")
+    public String listPosts(ModelMap page) {
+        String response;
+        response = "home";
+        page.put("title", "Blog Post 1");
+        return response;
+    }
 }
